8ae5a764affbb97397a9418599f96abdd5150233
suraj <surajkushwaha022@gmail.com>
2025-05-11 22:30:13 +0530

    added AppIcon AppName and Assets Images

--- lib/core/utils/Images.dart
+++ lib/core/utils/Images.dart
@@ -1,7 +1,7 @@
 class Images {
-  static const String rain = 'assets/images/rain.jpg';
+  static const String rain = 'assets/images/rain.png';
   static const String mist = 'assets/images/mist.jpg';
-  static const String snow = 'assets/images/snow.jpg';
+  static const String snow = 'assets/images/snow.png';
   static const String storm = 'assets/images/storm.jpg';
   static const String sunny = 'assets/images/sunny.png';
   static const String cloud = 'assets/images/cloud.jpg';
